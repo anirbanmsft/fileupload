d8bd22bf8ef8dcdbbe325133332e4241cf945b60
Anirban Bhattacherji <anirbanbh@microsoft.com>
2022-09-05 17:37:26 +0100

    MP4 to AVI converter added

--- src/main/java/com/azure/fileupload/service/BlobServiceImpl.java
+++ src/main/java/com/azure/fileupload/service/BlobServiceImpl.java
@@ -1,11 +1,13 @@
 package com.azure.fileupload.service;
 
-import java.io.IOException;
+import java.io.File;
+import java.nio.file.Files;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,13 +27,16 @@ public class BlobServiceImpl implements BlobService {
     @Autowired
     private StorageProperty storageProperty;
     
+    @Autowired
+	private ConverterService mp4ToAviConverterServiceImpl;
+    
     Logger logger = LogManager.getLogger(BlobServiceImpl.class);
 
 	@Override
-	public ContainerBlobResponse writeBlobFile(MultipartFile file) throws NoSuchAlgorithmException, IOException {
+	public ContainerBlobResponse writeBlobFile(MultipartFile file) throws Exception {
 		MessageDigest md5Digest = MessageDigest.getInstance("MD5");
     	String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
-    	String blobname = HashCreator.getFileChecksum(md5Digest, timeStamp + file.getName());
+    	String blobname = HashCreator.getFileChecksum(md5Digest, timeStamp + file.getOriginalFilename());
     	
     	logger.info("azure.storage.account-name: " + storageProperty.getAccountName());
     	logger.info("azure.storage.container-name: " + storageProperty.getContainerName());
@@ -44,15 +49,24 @@ public class BlobServiceImpl implements BlobService {
     	
         BlobClient blob;
 		try {
+			
 			BlobContainerClient container = new BlobContainerClientBuilder()
 					.connectionString(connectionString)
 					.containerName(storageProperty.getContainerName())
 					.buildClient();
 			logger.info("container.getBlobContainerName: " + container.getBlobContainerName());
 			logger.info("container.getBlobContainerUrl: " + container.getBlobContainerUrl());
+			logger.info("MultipartFile name: " + file.getOriginalFilename());
+			logger.info("MultipartFile Extension: " + FilenameUtils.getExtension(file.getOriginalFilename()));
 			blob = container.getBlobClient(blobname);
-						
-			blob.upload(file.getInputStream(), file.getSize(), true);
+			if(FilenameUtils.getExtension(file.getOriginalFilename()).equalsIgnoreCase("mp4")) {
+				File aviFile = mp4ToAviConverterServiceImpl.convert(file, blobname);
+				logger.info("AVI File name: " + aviFile.getName());
+				logger.info("AVI File path: " + aviFile.toPath());
+				blob.upload(FileUtils.openInputStream(aviFile), Files.size(aviFile.toPath()), true);
+			} else {
+				blob.upload(file.getInputStream(), file.getSize(), true);
+			}
 		} catch (Exception e) {
 			logger.error(e);
 			throw e;
